bfc4ab27d031fe22bf71f8db01b48f1cf0d8d362
Jason365 <brestel.jason@gmail.com>
2015-02-05 16:37:31 -0700

    Made a ship and made it move
    Also added collisions

--- Unity Project Files/Assets/PlayerMovement.cs
+++ Unity Project Files/Assets/PlayerMovement.cs
@@ -3,13 +3,50 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
 
-	// Use this for initialization
+	float maxSpeed = 20f;
+
+	float shipBoundaryRadius = 1.8f;
+
 	void Start () {
 	
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
-	
+
+
+
+		//Movement
+		Vector3 pos = transform.position;
+
+		pos.x += Input.GetAxis("Horizontal") * maxSpeed * Time.deltaTime;
+
+		pos.y += Input.GetAxis("Vertical") * maxSpeed * Time.deltaTime;
+
+		transform.position = pos;
+
+		//Restricting Camera Boundaries
+
+		if (pos.y + shipBoundaryRadius > Camera.main.orthographicSize) {
+			pos.y = Camera.main.orthographicSize - shipBoundaryRadius;
+		}
+		if (pos.y - shipBoundaryRadius < -Camera.main.orthographicSize) {
+			pos.y = -Camera.main.orthographicSize + shipBoundaryRadius;
+		}
+
+		float screenRatio = (float)Screen.width / (float)Screen.height;
+		float widthOrtho = Camera.main.orthographicSize * screenRatio;
+
+		if (pos.x + shipBoundaryRadius > widthOrtho) {
+			pos.x = widthOrtho - shipBoundaryRadius;
+		}
+		if (pos.x - shipBoundaryRadius < -widthOrtho) {
+			pos.x = -widthOrtho + shipBoundaryRadius;
+		}
+
+
+
+
+		transform.position = pos;
+
 	}
 }
