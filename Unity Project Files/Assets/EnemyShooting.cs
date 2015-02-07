54c4befd0979cd3141d02ebf5b9086733835c092
Jason365 <brestel.jason@gmail.com>
2015-02-06 17:47:24 -0700

    Added player spawning and lives
    lives are in the top left

---
+++ Unity Project Files/Assets/EnemyShooting.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyShooting : MonoBehaviour {
+	
+	public Vector3 bulletOffset = new Vector3 (0, 0.5f, 0);
+	
+	public GameObject bulletPrefab;
+	
+	public float fireDelay = 1f;
+	float cooldownTimer = 0;
+	
+	void Update () {
+		cooldownTimer -= Time.deltaTime;
+		
+		if (cooldownTimer <= 0) {
+			//Shoot Pew pew pew
+
+			cooldownTimer = fireDelay;
+			
+			Vector3 offset = transform.rotation * bulletOffset;
+			
+			Instantiate(bulletPrefab, transform.position + offset, transform.rotation);
+			
+		}
+	}
+}
