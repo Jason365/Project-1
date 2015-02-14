6e2fb84e4c1049534b386e840e3bbc66bb0ed5ea
Jason365 <brestel.jason@gmail.com>
2015-02-13 21:48:59 -0700

    Lots of changes to stuff

---
+++ Unity Project Files/Assets/TripleShot.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TripleShot : MonoBehaviour {
+	
+	public GameObject bulletPrefab;
+	public Vector3 bulletOffset = new Vector3 (0, 0.5f, 0);
+	public float fireDelay = 1f;
+	float cooldownTimer = 0;
+	public Quaternion bulletRotation1;
+	public Quaternion bulletRotation2;
+
+
+	void Update () {
+		cooldownTimer -= Time.deltaTime;
+		
+		if (cooldownTimer <= 0) {
+			//Shoot Pew pew pew
+			
+			cooldownTimer = fireDelay;
+			
+			Vector3 offset = transform.rotation * bulletOffset;
+
+			Instantiate(bulletPrefab, transform.position + offset, transform.rotation);
+			Instantiate(bulletPrefab, transform.position + offset, transform.rotation * bulletRotation1);
+			Instantiate(bulletPrefab, transform.position + offset, transform.rotation * bulletRotation2);
+
+		}
+	}
+}
